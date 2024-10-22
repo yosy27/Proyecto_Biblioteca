4736694ada5269811b86628b4e860dea348f1384
Yoselyn Cántaro <yoselynespinoza27@gmail.com>
2024-10-22 16:08:02 -0500

    Estructura inicial del Proyecto

---
+++ src/main/java/pe/com/cibertec/Proyecto_biblioteca/ProyectoBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.Proyecto_biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/cibertec/Proyecto_biblioteca/ProyectoBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.Proyecto_biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
